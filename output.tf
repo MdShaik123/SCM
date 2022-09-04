76c6888661713106bafdd8d413eb9c672a190270
Mohammed Abdul Shaik <mohshaik@amdocs.com>
2022-09-04 14:14:03 +0530

    hello

---
+++ output.tf
@@ -0,0 +1,12 @@
+output "zones" {
+  value = data.aws_availability_zones.available.names
+}
+
+output "vpcid" {
+    value = aws_vpc.vpc.id
+}
+
+output "countOfAZ" {
+    value = length(data.aws_availability_zones.available.names)
+  
+}
